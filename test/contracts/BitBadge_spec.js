db883d9378f230678fc6fed61cfd73226b693101
Joseph Pentland <joe.pentland@gmail.com>
2015-10-21 07:29:19 +0200

    tests: Remove old "bitbadge" test

--- test/contracts/BitBadge_spec.js
+++
@@ -1,8 +0,0 @@
-Embark = require('embark-framework');
-Embark.init();
-Embark.blockchainConfig.loadConfigFile('config/blockchain.yml');
-Embark.contractsConfig.loadConfigFile('config/contracts.yml');
-
-var files = ['app/contracts/BitBadge.sol'];
-Embark.contractsConfig.init(files, 'development');
-
